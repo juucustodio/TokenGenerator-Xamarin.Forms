aee980896f4bd757f1f13a6ec011d4fe4967777a
Juliano Custodio <juliano.custodio@hotmail.com.br>
2018-10-08 00:09:53 -0300

    ✨ Implement Token Generator

--- DemoToken/DemoToken/DemoToken/MainPage.xaml.cs
+++ DemoToken/DemoToken/DemoToken/MainPage.xaml.cs
@@ -1,17 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using OtpSharp;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using Xamarin.Forms;
 
 namespace DemoToken
 {
     public partial class MainPage : ContentPage
     {
+        public string Codigo
+        {
+            get => _codigo;
+            set
+            {
+                _codigo = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _codigo;
+
+        public string Segundos
+        {
+            get => _segundos;
+            set
+            {
+                _segundos = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _segundos;
+
+        private readonly byte[] secretKey;
+        private Totp totp;
+        private Timer timer;
+
         public MainPage()
         {
+            //Identificador
+            string Id = "100";
+
             InitializeComponent();
+            BindingContext = this;
+
+            secretKey = Encoding.UTF8.GetBytes(Id);
+        }
+
+        public void Callback(object state)
+        {
+            totp = new Totp(secretKey);
+
+            Codigo = totp.ComputeTotp();
+            var remainingTime = totp.RemainingSeconds();
+            Segundos = $"00:{remainingTime:00}";
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            timer = new Timer(Callback, null, 0, 1000);
+        }
+
+        protected override void OnDisappearing()
+        {
+            timer.Dispose();
+            base.OnDisappearing();
         }
     }
 }
